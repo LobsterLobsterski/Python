05467ffc134df900245b3f277ef99234ae988f1c
Tomasz Potoczko <100423134+LobsterLobsterski@users.noreply.github.com>
2022-09-09 11:42:17 +0200

    Add files via upload

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,47 +1,57 @@
-import sys
-import pygame as pg
 import random
+import sys
+
+import pygame as pg
+
 from shape import Shape
 
+'''
+Made by Tomasz Potoczko. 
+This project is a simple incarnation of 
+Tetris made using Pygame.
+'''
 
-def stain(grid_array, tetrino):
+
+# global variables
+score = 0
+prev_tetris = False
+
+
+def update_grid_array(grid_array, tetrino):
     # this method will 'stain' the grid with all the tetrinos which have reached the bottom
-    print(tetrino.shape)
-    print(len(tetrino.shape))
-    counter = 0
-    average_x = 0
-    average_y = 0
-    for coordinates in tetrino.shape:
-        x, y = coordinates[0]//30, coordinates[1]//30
-        # print(f"x:{x} y:{y}")
-        if counter < 3:
-            counter += 1
-            average_x += x
-            average_y += y
-        elif counter == 3:
-            average_x = (average_x + x) // 4
-            average_y = (average_y + y) // 4
-            print(f"average_x:{average_x}, average_y:{average_y}")
-            counter = 0
-            average_x, average_y = 0, 0
+    tetrino_grid_coords = tetrino.grid_coords[tetrino.rotation % len(tetrino.grid_coords)]
+    for coordinates in tetrino_grid_coords:
+        x, y = coordinates[0], coordinates[1]
+        grid_array[y][x] = tetrino.colour
+
     return grid_array
 
 
 def get_rand_shape():
     rand_shape = random.choice(["I", "J", "L", "O", "S", "T", "Z"])
-    print(rand_shape)
     return Shape(rand_shape)
+
+
+def update_screen_with_old_shapes(window, grid_array):
+    # goes through the grid_array and checks if a position is not of the background colour
+    # if it isn't it draws a rect there of the color that is there instead
+    for row_idx, row in enumerate(grid_array):
+        for col_idx, colour in enumerate(row):
+            if colour != (100, 100, 100):
+                pg.draw.rect(window, colour, (col_idx * 30, row_idx * 30, 30, 30))
 
 
 def make_grid(window, width, height, rows, cols):
     # draws a grid on the screen
-    distance_btw_rows = width//rows
-    distance_btw_cols = height//cols
+    distance_btw_rows = width // rows
+    distance_btw_cols = height // cols
     x, y = 0, 0
+    # draws vertical lines
     for i in range(rows):
         x += distance_btw_rows
         pg.draw.line(window, (0, 0, 0), (x, 0), (x, height))
 
+    # draws horizontal lines
     for i in range(cols):
         y += distance_btw_cols
         pg.draw.line(window, (0, 0, 0), (0, y), (width, y))
@@ -52,34 +62,122 @@ def make_shape(window, dx, dy, tetrino):
     pg.draw.polygon(window, tetrino.colour, tetrino.shape)
 
 
-def rotate(window, tetrino):
-    tetrino.rotate(270)
+def moveable_directions(tetrino, grid_array):
+    # checks if the spaces to the right, left and below are available (coloured gray)
+    directions = [1, 1, 1]  # left, right, down
+    tetrino_grid_coords = tetrino.grid_coords[tetrino.rotation % len(tetrino.grid_coords)]
+
+    # check if left squares are empty
+    try:
+        for coord in tetrino_grid_coords:
+            if grid_array[coord[1]][coord[0] - 1] != (100, 100, 100):
+                directions[0] = 0
+                break
+    except IndexError:
+        directions[0] = 0
+
+    # check if right squares are empty
+    try:
+        for coord in tetrino_grid_coords:
+            if grid_array[coord[1]][coord[0] + 1] != (100, 100, 100):
+                directions[1] = 0
+                break
+    except IndexError:
+        directions[1] = 0
+    # check if down squares are empty
+    try:
+        for coord in tetrino_grid_coords:
+            if grid_array[coord[1] + 1][coord[0]] != (100, 100, 100):
+                directions[2] = 0
+                break
+    except IndexError:
+        # we pass because we want the code to run for one more square down
+        # as to allow the player to move left and right just before the shape
+        # is set
+        pass
+
+    return directions
+
+
+def remove_full_bar(grid_array):
+    # when a row is full it will delete that row
+    global score, prev_tetris
+    rows_deleted = 0
+    add_score = 0
+    for idx, row in enumerate(grid_array):
+        remove_row = True
+
+        for colour in row:
+            if colour == (100, 100, 100):
+                remove_row = False
+                break
+
+        if remove_row:
+            # the row above becomes the considered row
+            # until we get to the top and then top is set ro gray
+            for i in range(idx, 1, -1):
+                grid_array[i] = grid_array[i - 1]
+            grid_array[0] = [(100, 100, 100) for _ in range(10)]
+
+            add_score += 100
+            rows_deleted += 1
+            if rows_deleted == 4:
+                if not prev_tetris:
+                    add_score = 800
+                else:
+                    add_score = 1200
+
+                prev_tetris = True
+
+            else:
+                prev_tetris = False
+
+    # print(prev_tetris)
+    # print(f'add_score {add_score}')
+    score += add_score
+
+
+def display_next_shape(window, tetrino):
+    if not tetrino.shape:
+        tetrino.make_shape(260, 450)
     pg.draw.polygon(window, tetrino.colour, tetrino.shape)
 
+
 def main():
+    global score
     # setting the window
     pg.init()
     size = width, height = 300, 600
     rows, cols = 10, 20
-    window = pg.display.set_mode(size)
+    window = pg.display.set_mode((500, 600))
     pg.display.set_caption("TETRIS")
+    img = pg.image.load("data/logo.png").convert()
+    pg.display.set_icon(img)
 
     # setting the fps
     clock = pg.time.Clock()
     clock.tick(60)
 
     # game initialisation
-    grid_array = [[0 for _ in range(rows)] for _ in range(cols)]
+    grid_array = [[(100, 100, 100) for _ in range(10)] for _ in range(20)]
     square = 30  # used to denote the distance
     counter = 0  # used for counting frames
     current_shape = get_rand_shape()
     next_shape = get_rand_shape()
+    moves = [1, 1, 1]
+    font = pg.font.SysFont('Comic Sans MS', 25)
+    next_shape_text = font.render('NEXT SHAPE: ', False, (0, 0, 0))
+    restart_text = font.render('PRESS ANY BUTTON TO RESTART', False, (0, 0, 0))
+
     # game loop
     while 1:
+        # break
         dx, dy = 0, 0
+        score_text = font.render(f'Score: {score}', False, (0, 0, 0))
+        level_text = font.render(f'Level: {score // 500 + 1}', False, (0, 0, 0))
+
+        # get user input
         for event in pg.event.get():
-            if event.type == pg.USEREVENT:
-                print(event)
             if event.type == pg.QUIT:
                 sys.exit()
             if event.type == pg.KEYDOWN:
@@ -89,33 +187,71 @@ def main():
                     dx -= square
                 if event.key == pg.K_DOWN:
                     dy += square
+                    score += 1
                 if event.key == pg.K_UP:
                     # ROTATE
-                    print(F"current_shape.shape before\n{current_shape.shape}")
-                    current_shape.rotate(90)
-                    print(F"current_shape.shape after\n{current_shape.shape}")
+                    current_shape.rotate(grid_array)
+
+        # every second or so the shape will drop by one square
+        # they start to fall faster the further we are in the game
+        if counter > 1000 - (score // 500 + 1) * 100:
+            counter = 0
+            dy += square
+
+        # collision with colour blocks
+        if counter > 0:
+            moves = moveable_directions(current_shape, grid_array)
 
         # making sure we don't step over the window
-        if current_shape.x < 0 or current_shape.x > width-square:
-            current_shape.x = 0 if current_shape.x < 0 else width-square
+        if current_shape.left < square or not moves[0]:
+            if dx < 0:
+                dx = 0
 
-        if current_shape.y > height-current_shape.height:
-            # get new shape object and set it to the controlled on
-            # grid_array = stain(grid_array, current_shape)
+        if current_shape.right > width - square or not moves[1]:
+            if dx > 0:
+                dx = 0
+
+        if not moves[2] and current_shape.top < 120:
+            next_shape.x, next_shape.y = 4 * square, 30
+            make_shape(window, 0, 0, next_shape)
+            break
+
+        # when we hit the bottom
+        if current_shape.down > height or not moves[2]:
+            # get new shape object and set it to the controlled one
+            grid_array = update_grid_array(grid_array, current_shape)
+            remove_full_bar(grid_array)
+            next_shape.x, next_shape.y = 4 * square, 0
+            next_shape.shape = []
             current_shape = next_shape
             next_shape = get_rand_shape()
-            current_shape.x, current_shape.y = 4*square, 0
-
-        # every second the shape will drop by one square
-        if counter > 1000:
+            current_shape.x, current_shape.y = 4 * square, 0
             counter = 0
-            # dy += square
 
         window.fill((100, 100, 100))
+        update_screen_with_old_shapes(window, grid_array)
         make_shape(window, dx, dy, current_shape)
+        display_next_shape(window, next_shape)
         make_grid(window, width, height, rows, cols)
         counter += 1
         dx, dy = 0, 0
+        window.blit(next_shape_text, (330, 400))
+        window.blit(score_text, (330, 0))
+        window.blit(level_text, (330, 200))
+        pg.display.update()
+
+    font = pg.font.SysFont('Comic Sans MS', 80)
+    game_over_text = font.render('GAME OVER', False, (0, 0, 0))
+
+    while 2:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                main()
+        window.blit(game_over_text, (25, 200))
+        window.blit(restart_text, (50, 300))
+
         pg.display.update()
 
 
--- Tetris/shape.py
+++ Tetris/shape.py
@@ -1,74 +1,130 @@
-import math
-
-
 class Shape:
     def __init__(self, type):
         self.type = type  # dictates the shape
         self.shape = []
         self.colour = ()
-        self.height = 0
         # setting starting coords
         self.x = 4*30
         self.y = 0
-        #  just like with rect object
+        # just like with rect object
         self.top = 0
         self.right = 0
         self.down = 0
         self.left = 0
+        # used for rotation
+        self.grid_coords = []
+        self.rotation = 0
 
-    def init_trdl(self):
-        # initialises the top, right, down and left values based on the shape
-        pass
+    def set_trdl(self):
+        # sets up the top, right, left and down attributes (same as in rect object of pygame)
+        self.top = min([coord[1] for coord in self.shape])
+        self.right = max([coord[0] for coord in self.shape])
+        self.down = max([coord[1] for coord in self.shape])
+        self.left = min([coord[0] for coord in self.shape])
+
+    def get_grid_coords(self):
+        # setting up the points occupied by the shape from the point of view of the board
+
+        point_x, point_y = self.left // 30, self.top // 30
+        if self.type == 'I':
+            self.grid_coords = [
+                [[point_x, point_y], [point_x, point_y + 1], [point_x, point_y + 2], [point_x, point_y + 3]],
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 2, point_y], [point_x + 3, point_y]]
+            ]
+        elif self.type == 'J':
+            self.grid_coords = [
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 2, point_y], [point_x + 2, point_y + 1]],
+                [[point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x + 1, point_y + 2],
+                 [point_x, point_y + 2]],
+                [[point_x, point_y], [point_x, point_y + 1], [point_x + 1, point_y + 1], [point_x + 2, point_y + 1]],
+                [[point_x, point_y], [point_x + 1, point_y], [point_x, point_y + 1], [point_x, point_y + 2]]
+            ]
+            # as a not the 3rd one if offset to the right by one
+        elif self.type == 'L':
+            self.grid_coords = [
+                [[point_x + 2, point_y], [point_x + 2, point_y + 1], [point_x + 1, point_y + 1],
+                 [point_x, point_y + 1]],
+                [[point_x, point_y], [point_x, point_y + 1], [point_x, point_y + 2], [point_x + 1, point_y + 2]],
+                [[point_x, point_y + 1], [point_x, point_y], [point_x + 1, point_y], [point_x + 2, point_y]],
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x + 1, point_y + 2]]
+            ]
+        elif self.type == 'O':
+            self.grid_coords = [
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x, point_y + 1]]
+            ]
+        elif self.type == 'S':
+            self.grid_coords = [
+                [[point_x, point_y], [point_x, point_y + 1], [point_x + 1, point_y + 1], [point_x + 1, point_y + 2]],
+                [[point_x + 1, point_y], [point_x + 2, point_y], [point_x + 1, point_y + 1], [point_x, point_y + 1]]
+            ]
+        elif self.type == 'T':
+            self.grid_coords = [
+                [[point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x, point_y + 1],
+                 [point_x + 2, point_y + 1]],
+                [[point_x, point_y], [point_x, point_y + 1], [point_x, point_y + 2], [point_x + 1, point_y + 1]],
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 2, point_y], [point_x + 1, point_y + 1]],
+                [[point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x, point_y + 1], [point_x + 1, point_y + 2]]
+            ]
+        elif self.type == 'Z':
+            self.grid_coords = [
+                [[point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x, point_y + 1], [point_x, point_y + 2]],
+                [[point_x, point_y], [point_x + 1, point_y], [point_x + 1, point_y + 1], [point_x + 2, point_y + 1]]
+            ]
 
     def make_shape(self, dx, dy):
+        # this function creates a polygon object depending on the type and coordinates
         self.x += dx
         self.y += dy
-        # this function creates a polygon object depending on the type
 
         # if not initialised yet set the values
         if not self.shape:
             list_of_points = []  # the list fo points needed to draw the polygon
             colour = 0, 0, 0  # the colour values for a shape
 
-            self.type = 'I'  # for testing
+            # self.type = 'I'  # for testing
 
             if self.type == 'I':
                 # the 1 by 4
                 colour = 240, 248, 255
                 list_of_points = [[self.x, self.y], [self.x+30, self.y], [self.x+30, self.y+120], [self.x, self.y+120]]
-                self.height = 4*30
             elif self.type == 'J':
                 # the other-way-around L
                 colour = 3, 37, 126
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+60, self.y), (self.x+90, self.y), (self.x+90, self.y+30), (self.x+90, self.y+60), (self.x+60, self.y+60), (self.x+60, self.y+30), (self.x+30, self.y+30), (self.x, self.y+30)]
-                self.height = 2*30
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+60, self.y), (self.x+90, self.y),
+                                  (self.x+90, self.y+30), (self.x+90, self.y+60), (self.x+60, self.y+60),
+                                  (self.x+60, self.y+30), (self.x+30, self.y+30), (self.x, self.y+30)]
             elif self.type == 'L':
                 # the L
                 colour = 255, 165, 0
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+30, self.y+60), (self.x, self.y+60), (self.x-30, self.y+60), (self.x-60, self.y+60), (self.x-60, self.y+30),(self.x-30, self.y+30), (self.x, self.y+30)]
-                self.height = 2*30
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+30, self.y+60),
+                                  (self.x, self.y+60), (self.x-30, self.y+60), (self.x-60, self.y+60),
+                                  (self.x-60, self.y+30),(self.x-30, self.y+30), (self.x, self.y+30)]
             elif self.type == 'O':
                 # the square
                 colour = 255, 255, 0
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+60, self.y), (self.x+60, self.y+30), (self.x+60, self.y+60), (self.x+30, self.y+60), (self.x, self.y+60), (self.x, self.y+30)]
-                self.height = 2*30
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+60, self.y), (self.x+60, self.y+30),
+                                  (self.x+60, self.y+60), (self.x+30, self.y+60), (self.x, self.y+60),
+                                  (self.x, self.y+30)]
             elif self.type == 'S':
                 # the square with the left side 1 block lower
                 colour = 0, 255, 0
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+60, self.y+30), (self.x+60, self.y+60), (self.x+60, self.y+90), (self.x+30, self.y+90), (self.x+30, self.y+60), (self.x, self.y+60), (self.x, self.y+30)]
-                self.height = 3*30
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+60, self.y+30),
+                                  (self.x+60, self.y+60), (self.x+60, self.y+90), (self.x+30, self.y+90),
+                                  (self.x+30, self.y+60), (self.x, self.y+60), (self.x, self.y+30)]
             elif self.type == 'T':
                 # the T
                 colour = 221, 160, 221
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+60, self.y+30), (self.x+60, self.y+60), (self.x+30, self.y+60), (self.x, self.y+60), (self.x-30, self.y+60), (self.x-30, self.y+30), (self.x, self.y+30)]
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+60, self.y+30),
+                                  (self.x+60, self.y+60), (self.x+30, self.y+60), (self.x, self.y+60),
+                                  (self.x-30, self.y+60), (self.x-30, self.y+30), (self.x, self.y+30)]
             elif self.type == 'Z':
                 # the square with the right side 1 block lower
                 colour = 255, 0, 0
-                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+30, self.y+60), (self.x, self.y+60), (self.x, self.y+90), (self.x-30, self.y+90), (self.x-30, self.y+60), (self.x-30, self.y+30), (self.x, self.y+30)]
-                self.height = 3*30
+                list_of_points = [(self.x, self.y), (self.x+30, self.y), (self.x+30, self.y+30), (self.x+30, self.y+60),
+                                  (self.x, self.y+60), (self.x, self.y+90), (self.x-30, self.y+90),
+                                  (self.x-30, self.y+60), (self.x-30, self.y+30), (self.x, self.y+30)]
 
             self.shape = list_of_points
-            self.init_trdl()
             self.colour = colour
 
         # otherwise we can't set out shape but operate and mutate it
@@ -80,31 +136,56 @@ class Shape:
 
             self.shape = new
 
-    def rotate(self, angle):
-        # fix the elongation effect
-        angle = math.radians(angle)
-        for _ in range(2):
-            origin = self.shape[0]
-            print(origin)
-            rotated = []
-            for point in self.shape:
-                temp = point[0] - origin[0], point[1] - origin[1]
-                # print(f"temp0: {temp[0]} temp1: {temp[1]}")
-                temp = int(round(temp[0]*math.cos(angle) - temp[1]*math.sin(angle), -1)),\
-                    int(round(temp[0]*math.sin(angle) + temp[1]*math.cos(angle), -1))
+        # at the end we set helpful attributes
+        self.get_grid_coords()
+        self.set_trdl()
 
-                # print(temp)
-                rotated.append((point[0]+temp[0], point[1]+temp[1]))
+    def rotate(self, grid_array):
+        self.rotation += 1
+        origin = self.shape[0]
+        rotated = []
+        for point in self.shape:
+            # setting up the point around which we will rotate
+            temp = point[0] - origin[0], point[1] - origin[1]
+            # simplified rotation by 90 degrees
+            temp = -temp[1], temp[0]
+            # same as temp = int(round(temp[0]*math.cos(angle) - temp[1]*math.sin(angle), -1)),\
+            #     int(round(temp[0]*math.sin(angle) + temp[1]*math.cos(angle), -1))
+            # with angle of 90 degrees it does the exact same thing
+            rotated.append([temp[0]+origin[0], temp[1]+origin[1]])
 
-            self.shape = rotated
+        prev = self.shape  # needed to possibly revert the change
+        self.shape = rotated
+        self.set_trdl()
+        self.get_grid_coords()
+        self.x, self.y = self.shape[0]
 
+        # makes sure we don't rotate outside the screen
+        if self.left < 0:
+            for idx, point in enumerate(self.shape):
+                self.shape[idx][0] += -self.left
+        if self.right > 300:
+            for idx, point in enumerate(self.shape):
+                self.shape[idx][0] = self.shape[idx][0] + (300-self.right)
+        if self.down > 600:
+            for idx, point in enumerate(self.shape):
+                self.shape[idx][1] = self.shape[idx][1] + (600-self.down)
+        if self.top < 0:
+            for idx, point in enumerate(self.shape):
+                self.shape[idx][1] += -self.top
 
-if __name__ == '__main__':
-    obj = Shape('I')
-    obj.make_shape(0, 0)
-    print(f"before rotation cls.shape: \n{obj.shape}")
-    obj.rotate(90)
-    print(f"after rotation cls.shape: \n{obj.shape}")
+        self.set_trdl()
+        self.get_grid_coords()
+        self.x, self.y = self.shape[0]
 
+        # makes sure we don't rotate into another shape
+        tetrino_grid_coords = self.grid_coords[self.rotation % len(self.grid_coords)]
+        for coord in tetrino_grid_coords:
+            if grid_array[coord[1]][coord[0]] == (100, 100, 100):
+                continue
 
-
+            self.shape = prev
+            self.set_trdl()
+            self.get_grid_coords()
+            self.x, self.y = self.shape[0]
+            return
